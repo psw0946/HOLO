89931b6249bd16922d99cb84f8087b593e289e5c
psw0946 <psw0946@snu.ac.kr>
2018-10-14 20:47:15 +0900

    Spring: Configuration with files

---
+++ Spring/SpringProjectEx8-3-1/src/main/java/com/javalec/ex/MainClass.java
@@ -0,0 +1,33 @@
+package com.javalec.ex;
+
+import java.util.Scanner;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String config = null;
+		Scanner scanner = new Scanner(System.in);
+		String str = scanner.next();
+		if (str.equals("dev")) {
+			config = "dev";
+		} else if (str.equals("run")) {
+			config = "run";
+		}
+		
+		scanner.close();
+		
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+		ctx.getEnvironment().setActiveProfiles(config);
+		ctx.load("applicationCTX_dev.xml", "applicationCTX_run.xml");
+		
+		ServerInfo info = ctx.getBean("serverInfo", ServerInfo.class);
+		System.out.println("ip: " + info.getIpNum());
+		System.out.println("port: " + info.getPortNum());
+		
+		ctx.close();
+	}
+
+}
