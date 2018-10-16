e26d62384acdb7ef953b52ffc9a006561989e897
psw0946 <psw0946@snu.ac.kr>
2018-10-17 00:45:39 +0900

    Spring: AOP Basics

---
+++ Spring/SpringProjectEx9-2/src/main/java/com/javalec/ex/Worker.java
@@ -0,0 +1,32 @@
+package com.javalec.ex;
+
+public class Worker {
+	private String name;
+	private String age;
+	private String job;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	
+	public void getWorkerInfo() { // 핵심 기능에 해당함.
+		System.out.println("이름: " + getName());
+		System.out.println("나이: " + getAge());
+		System.out.println("직업: " + getJob());
+	}
+}
