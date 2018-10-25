6308852457ec00082b381690930568daf79ed6fb
psw0946 <psw0946@snu.ac.kr>
2018-10-26 00:32:51 +0900

    JAGU : Binary Search in C++

---
+++ JAGU/Search/Binary_Search/binsearch.h
@@ -0,0 +1,11 @@
+#ifndef BIN_SEARCH
+#define BIN_SEARCH
+
+#define TRUE 1
+#define FALSE 0
+#define FAIL -1
+
+int binarySearchRecursive(int value[], int start, int end, int key);
+int binarySearch(int value[], int start, int end, int key);
+
+#endif
