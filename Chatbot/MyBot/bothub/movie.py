ba8a430d834ddfc901dbecbc041e976893316a3d
psw0946 <psw0946@snu.ac.kr>
2018-10-28 21:19:52 +0900

    Chatbot : movie crawling error fixed (due to star crawling)

--- Chatbot/MyBot/bothub/movie.py
+++ Chatbot/MyBot/bothub/movie.py
@@ -5,12 +5,25 @@ import simplejson as json
 class Movie:
 
     def get_top10(self):
-        response = "네이버 영화 예매 순위 TOP10\n"
+        response = "네이버 영화 예매율 순위 TOP10\n"
 
         r = req.get('https://movie.naver.com/movie/sdb/rank/rreserve.nhn')
         soup = BeautifulSoup(r.text, 'html.parser')
 
-        names = soup.select('div.tit4 > a')
+        rankingTable = soup.select('table.list_ranking > tbody > tr')[1:-1]
+
+        for i, rank in enumerate(rankingTable, 1):
+            title = rank.select_one('div.tit4').text.strip()
+            reserve = rank.select_one('td.reserve_per.ar').text.strip()
+            res = str(i) + "위: " + title + " / " + reserve + "\n"
+            response += res
+
+        response += "\n국내 박스오피스 랭킹 TOP10\n"
+
+        r = req.get('https://movie.naver.com/movie/sdb/rank/rboxoffice.nhn')
+        soup = BeautifulSoup(r.text, 'html.parser')
+
+        names = soup.select('div.tit1 > a')
 
         for i, name in enumerate(names, 1):
             movie_name = name.text
