b75be0bbc9dac944c5b7003ddbd017b016abad73
psw0946 <psw0946@snu.ac.kr>
2018-11-16 23:28:17 +0900

    React : 작업환경 설정...(not finished)

---
+++ React/react-fundamentals/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// var React = require('react'); 와 같다.
+
+class App extends React.Component {
+  render() {
+    return (
+      <h1>Hello</h1>
+    );
+  }
+}
+
+export default App;
+// module.export = App; 와 같다.
