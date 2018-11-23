852be167ebd77a2f6c56897b29881beee58798fc
psw0946 <psw0946@snu.ac.kr>
2018-11-23 23:45:06 +0900

    React : react-redux 적용 ... with provider...

--- React/redux-example/src/components/Counter.js
+++ React/redux-example/src/components/Counter.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 import Value from './Value';
 import Control from './Control';
 
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
 const propTypes = {
 };
 
@@ -12,13 +15,34 @@ const defaultProps = {
 class Counter extends Component {
   constructor(props) {
     super(props);
+
+    this.setRandomColor = this.setRandomColor.bind(this);
+  }
+
+  setRandomColor() {
+    const color = [
+      Math.floor((Math.random()*55) + 200),
+      Math.floor((Math.random()*55) + 200),
+      Math.floor((Math.random()*55) + 200)
+    ];
+
+    this.props.handleSetColor(color);
   }
 
   render() {
+    const color = this.props.color;
+    const style = {
+      background: `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+    };
+
     return (
-      <div>
-        <Value />
-        <Control />
+      <div style={style}>
+        <Value number={this.props.number} />
+        <Control
+          onPlus={this.props.handleIncrement}
+          onSubtract={this.props.handleDecrement}
+          onRandomizeColor={this.setRandomColor}
+        />
       </div>
     );
   }
@@ -27,4 +51,19 @@ class Counter extends Component {
 Counter.propTypes = propTypes;
 Counter.defaultProps = defaultProps;
 
-export default Counter;
+const mapStateToProps = (state) => {
+  return {
+    number: state.counter.number,
+    color: state.ui.color
+  };
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleIncrement: () => { dispatch(actions.increment()) },
+    handleDecrement: () => { dispatch(actions.decrement()) },
+    handleSetColor: (color) => { dispatch(actions.setColor(color)) }
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- React/redux-example/src/index.js
+++ React/redux-example/src/index.js
@@ -3,8 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import reducers from './reducers';
+// import * as actions from './actions';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers);
+
+/*
+console.log(store.getState());
+const unsubscribe = store.subscribe(() => console.log(store.getState()));
+store.dispatch(actions.increment());
+store.dispatch(actions.increment());
+store.dispatch(actions.decrement());
+store.dispatch(actions.setColor([200, 200, 200]));
+
+unsubscribe(); // store의 subscribe 메소드가 unsubscribe하는 함수를 반환
+
+store.dispatch(actions.setColor([210, 210, 210]));
+*/
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
