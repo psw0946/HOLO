77bc0e6241cad5ce74be38bf75e6df035c8dc0f8
coma.park <psw0946@snu.ac.kr>
2019-09-23 00:36:16 +0900

    Javascript : 9-forkify - babel 및 polyfill 설정

--- Javascript/9-forkify/webpack.config.js
+++ Javascript/9-forkify/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path'); // built-in node module
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // src 에 있는 index.html 을 알아서 dist 로 복사해주도록
 
 module.exports = {
-    entry: './src/js/index.js', // where webpack will start the bundling
+    entry: ['@babel/polyfill', './src/js/index.js'], // where webpack will start the bundling
     output: {
         path: path.resolve(__dirname, 'dist'), // should be absolute path, __dirname: current directory, path 는 dist 까지만... dist 에 배포할 파일 다 들어있으므로..
         filename: 'js/bundle.js'
@@ -16,5 +16,16 @@ module.exports = {
             filename: 'index.html',
             template: './src/index.html' // 무엇을 복사할 것인가
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
 };
