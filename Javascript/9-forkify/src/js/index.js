cde9de43aad8d062adc7d8fbbea163d164cc910c
coma.park <psw0946@snu.ac.kr>
2019-10-03 19:36:29 +0900

    Javascript : 9-forkify - axios 를 이용한 api call

--- Javascript/9-forkify/src/js/index.js
+++ Javascript/9-forkify/src/js/index.js
@@ -1,6 +1,14 @@
-import str from './models/Search';
-// import { add as a, multiply as m, ID } from './views/searchView';
-import * as searchView from './views/searchView';
+import axios from 'axios';
 
-// console.log(`Using imported functions! ${a(ID, 2)} and ${m(3, 5)}. ${str}`);
-console.log(`Using imported functions! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3, 5)}. ${str}`);
+async function getResults(query) {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = '462b1cc8d4f2730081462fbc65136320';
+    try {
+        const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch (error) {
+        alert(error);
+    }
+}
+getResults('pizza');
