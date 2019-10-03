a6a061c6a8aa99ad023c21a82c6036e084f303ea
coma.park <psw0946@snu.ac.kr>
2019-10-03 19:14:56 +0900

    Javascript : 9-forkify - ES6 module 이 어떻게 쓰일 수 있는지

--- Javascript/9-forkify/src/js/index.js
+++ Javascript/9-forkify/src/js/index.js
@@ -1,5 +1,6 @@
-// Global app controller
-import num from './test';
+import str from './models/Search';
+// import { add as a, multiply as m, ID } from './views/searchView';
+import * as searchView from './views/searchView';
 
-const x = 23;
-console.log(`I imported ${num} from another module! Variable x is ${x}`);
+// console.log(`Using imported functions! ${a(ID, 2)} and ${m(3, 5)}. ${str}`);
+console.log(`Using imported functions! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3, 5)}. ${str}`);
