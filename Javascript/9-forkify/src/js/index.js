860a2275f15885796ee03de19a3e8ff6140d95b0
coma.park <psw0946@snu.ac.kr>
2019-10-06 23:56:02 +0900

    Javascript : 9-forkify - Building search model & controller

--- Javascript/9-forkify/src/js/index.js
+++ Javascript/9-forkify/src/js/index.js
@@ -1,14 +1,32 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const key = '462b1cc8d4f2730081462fbc65136320';
-    try {
-        const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = res.data.recipes;
-        console.log(recipes);
-    } catch (error) {
-        alert(error);
+/** Global state of the app
+ * - Search Object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
+
+const controlSearch = async () => {
+    // 1) Get query from view
+    const query = 'pizza'; // TODO
+
+    if (query) {
+        // 2) New search object and add to state
+        state.search = new Search(query);
+
+        // 3) Prepare UI for results
+
+        // 4) Search for recipes
+        await state.search.getResults();
+
+        // 5) Render results on UI
+        console.log(state.search.result);
     }
-}
-getResults('pizza');
+};
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
--- Javascript/9-forkify/src/js/models/Search.js
+++ Javascript/9-forkify/src/js/models/Search.js
@@ -1,1 +1,19 @@
-export default 'I am an exported string.';
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() { // every asynchronous function returns Promise.
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const key = 'a';
+        try {
+            const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            // console.log(this.result);
+        } catch (error) {
+            alert(error);
+        }
+    }
+}
