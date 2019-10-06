6543138acd21185d0d90ccd76f6926dc1b6505bc
coma.park <psw0946@snu.ac.kr>
2019-10-07 00:39:58 +0900

    Javascript : 9-forkify - tab size 2 로 맞추기

--- Javascript/9-forkify/src/js/index.js
+++ Javascript/9-forkify/src/js/index.js
@@ -11,26 +11,26 @@ import { elements } from "./views/base";
 const state = {};
 
 const controlSearch = async () => {
-    // 1) Get query from view
-    const query = searchView.getInput();
+  // 1) Get query from view
+  const query = searchView.getInput();
 
-    if (query) {
-        // 2) New search object and add to state
-        state.search = new Search(query);
+  if (query) {
+    // 2) New search object and add to state
+    state.search = new Search(query);
 
-        // 3) Prepare UI for results
-        searchView.clearInput();
-        searchView.clearResults();
+    // 3) Prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
 
-        // 4) Search for recipes
-        await state.search.getResults();
+    // 4) Search for recipes
+    await state.search.getResults();
 
-        // 5) Render results on UI
-        searchView.renderResults(state.search.result);
-    }
+    // 5) Render results on UI
+    searchView.renderResults(state.search.result);
+  }
 };
 
 elements.searchForm.addEventListener('submit', e => {
-    e.preventDefault();
-    controlSearch();
+  e.preventDefault();
+  controlSearch();
 });
--- Javascript/9-forkify/src/js/views/base.js
+++ Javascript/9-forkify/src/js/views/base.js
@@ -1,5 +1,9 @@
 export const elements = {
-    searchForm: document.querySelector('.search'),
-    searchInput: document.querySelector('.search__field'),
-    searchResultList: document.querySelector('.results__list'),
+  searchForm: document.querySelector('.search'),
+  searchInput: document.querySelector('.search__field'),
+  searchResultList: document.querySelector('.results__list'),
+};
+
+export const renderLoader = parent => {
+
 };
