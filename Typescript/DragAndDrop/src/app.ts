c979fa3ee328dfdde02e8173d7cb9acac48370a3
coma.park <psw0946@snu.ac.kr>
2020-03-18 00:40:35 +0900

    Typescript: Interacting with DOM Element

--- Typescript/DragAndDrop/src/app.ts
+++ Typescript/DragAndDrop/src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
 
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -9,7 +12,24 @@ class ProjectInput {
 
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element.id = 'user-input';
+
+        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+        this.descriptionElement = this.element.querySelector('#description') as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+        this.configure();
         this.attatch();
+    }
+
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
+    }
+
+    private configure() {
+        // bind(this) 를 안해주면 submitHandler 의 this 는 event 를 가리킨다.
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
     }
 
     private attatch() {
