8b4ef1bb1c4cde524ae6fe5d45957496778f2df8
coma.park <psw0946@snu.ac.kr>
2020-03-18 01:11:37 +0900

    Typescript: Get User Input

--- Typescript/DragAndDrop/src/app.ts
+++ Typescript/DragAndDrop/src/app.ts
@@ -34,10 +34,34 @@ class ProjectInput {
         this.attatch();
     }
 
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+
+        if (enteredTitle.trim().length === 0 || enteredDescription.trim().length === 0 || enteredPeople.trim().length === 0) {
+            alert('Invalid input, please try again!');
+            return;
+        } else {
+            return [enteredTitle, enteredDescription, +enteredPeople]
+        }
+    }
+
+    private clearInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionElement.value = '';
+        this.peopleInputElement.value = '';
+    }
+
     @autobind
     private submitHandler(event: Event) {
         event.preventDefault();
-        console.log(this.titleInputElement.value);
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, description, people] = userInput;
+            console.log(title, description, people);
+            this.clearInputs();
+        }
     }
 
     private configure() {
