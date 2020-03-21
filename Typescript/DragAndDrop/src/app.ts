03abc1e433a8435bc7e4c94dfff7ff38d323b577
coma.park <psw0946@snu.ac.kr>
2020-03-21 22:46:13 +0900

    Typescript: Create New Class - Project Item

--- Typescript/DragAndDrop/src/app.ts
+++ Typescript/DragAndDrop/src/app.ts
@@ -113,6 +113,26 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
     abstract renderContent(): void;
 }
 
+// ProjectItem Class
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+    private project: Project;
+
+    constructor(hostId: string, project: Project) {
+        super('single-project', hostId, false, project.id);
+        this.project = project;
+
+        this.configure();
+        this.renderContent();
+    }
+
+    configure() {}
+
+    renderContent() {
+        this.element.querySelector('h2')!.textContent = this.project.title;
+        this.element.querySelector('h3')!.textContent = this.project.people.toString();
+        this.element.querySelector('p')!.textContent = this.project.description;
+    }
+}
 
 // ProjectList Class
 class ProjectList extends Component<HTMLDivElement, HTMLElement>{
@@ -149,9 +169,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement>{
         const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
         listEl.innerHTML = '';
         for (const prjItem of this.assignedProjects) {
-            const listItem = document.createElement('li');
-            listItem.textContent = prjItem.title;
-            listEl.appendChild(listItem);
+            new ProjectItem(this.element.querySelector('ul')!.id, prjItem);
         }
     }
 }
@@ -199,10 +217,10 @@ class ProjectInput extends Component<HTMLDivElement, HTMLFormElement>{
         };
 
         if (validate(titleValidatable) && validate(descriptionValidatable) && validate(peopleValidatable)) {
+            return [enteredTitle, enteredDescription, +enteredPeople]
+        } else {
             alert('Invalid input, please try again!');
             return;
-        } else {
-            return [enteredTitle, enteredDescription, +enteredPeople]
         }
     }
 
