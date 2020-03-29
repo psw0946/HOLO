cf4db0a4ab93c760cd9eeb2c76b9d04a9b4ffc93
coma.park <psw0946@snu.ac.kr>
2020-03-29 17:14:00 +0900

    Typescript: Adding Development Workflow for Webpack

--- Typescript/DragAndDrop/webpack.config.js
+++ Typescript/DragAndDrop/webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/app.ts',
   output: {
     filename: 'bundle.js', // This will be the single javascript file which will be produced in the end.
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'dist'
   },
   devtool: 'inline-source-map', // sourceMap: true 와 같이 브라우저에서 디버깅 쉽게 하도록 해줌.
   module: {
