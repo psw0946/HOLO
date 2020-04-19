be61b7b18cfb493b8e364324bf00f65bef5e19d1
coma.park <psw0946@snu.ac.kr>
2020-04-19 18:10:47 +0900

    React: WebRequest - Update State with ComponentDidUpdate

--- React/WebRequest/src/components/FullPost/FullPost.js
+++ React/WebRequest/src/components/FullPost/FullPost.js
@@ -1,22 +1,43 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
+  state = {
+    loadedPost: null
+  }
 
-        );
-        return post;
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (this.props.id) {
+      // 이전 것과 비교해서 업데이트 해야한다. 하지 않으면 state 가 업데이트 됨에 따라 무한 루프에 빠짐
+      if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+        axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+          .then(response => {
+            this.setState({loadedPost: response.data});
+          });
+      }
     }
+  }
+
+  render () {
+    let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
+    if (this.props.id) {
+      post = <p style={{textAlign: 'center'}}>Loading...!</p>;
+    }
+    if (this.state.loadedPost) {
+      post = (
+        <div className="FullPost">
+          <h1>{this.state.loadedPost.title}</h1>
+          <p>{this.state.loadedPost.content}</p>
+          <div className="Edit">
+            <button className="Delete">Delete</button>
+          </div>
+        </div>
+      );
+    }
+    return post;
+  }
 }
 
 export default FullPost;
