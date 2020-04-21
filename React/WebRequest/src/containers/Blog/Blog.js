4fbff57d137271371fe423cfb07e9c2e686fb11e
coma.park <psw0946@snu.ac.kr>
2020-04-21 22:17:14 +0900

    React: Routing - Styling the Active Route with NavLink

--- React/WebRequest/src/containers/Blog/Blog.js
+++ React/WebRequest/src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -12,12 +12,22 @@ class Blog extends Component {
         <header>
           <nav>
             <ul>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to={{
+              <li><NavLink
+                to="/"
+                exact
+                activeClassName="my-active"
+                activeStyle={{
+                  color: '#fa923f',
+                  textDecoration: 'underline'
+                }}
+              >
+                Home
+              </NavLink></li>
+              <li><NavLink to={{
                 pathname: '/new-post',
                 hash: '#submit', // id 가 submit 인 곳으로 이동
                 search: '?quick-submit=true' // query param
-              }}>New Post</Link></li>
+              }}>New Post</NavLink></li>
             </ul>
           </nav>
         </header>
