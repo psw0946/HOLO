04b85e3f20735a43d918c1bc8baf15f744250110
coma.park <psw0946@snu.ac.kr>
2020-04-25 21:12:25 +0900

    React: Routing - Using Switch and History

--- React/WebRequest/src/containers/Blog/Blog.js
+++ React/WebRequest/src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -35,8 +35,10 @@ class Blog extends Component {
         {/*<Route path="/" exact render={() => <h1>Home</h1>} />*/}
         {/*<Route path="/" render={() => <h1>Home 2</h1>} />*/}
         <Route path="/" exact component={Posts} />
-        <Route path="/new-post" component={NewPost} />
-        <Route path="/:id" exact component={FullPost} />
+        <Switch> {/* 하나 해당되면 거기서 멈추고 더 이상 탐색하지 않음. */}
+          <Route path="/new-post" component={NewPost} />
+          <Route path="/:id" exact component={FullPost} />
+        </Switch>
       </div>
     );
   }
