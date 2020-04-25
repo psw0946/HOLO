27cd179f9e737311e1b989973a77930aafdd396c
coma.park <psw0946@snu.ac.kr>
2020-04-26 01:38:34 +0900

    React: Routing - Lazy Loading with Routes (Legacy)
    최신은 React.lazy(() => import('')) 를 사용

--- React/WebRequest/src/containers/Blog/Blog.js
+++ React/WebRequest/src/containers/Blog/Blog.js
@@ -3,9 +3,18 @@ import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+// import NewPost from './NewPost/NewPost';
+
+const AsyncNewPost = asyncComponent(() => {
+  return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
+  state = {
+    auth: true,
+  }
+
   render () {
     return (
       <div className="Blog">
@@ -34,9 +43,10 @@ class Blog extends Component {
         {/*<Route path="/" exact render={() => <h1>Home</h1>} />*/}
         {/*<Route path="/" render={() => <h1>Home 2</h1>} />*/}
         <Switch> {/* 하나 해당되면 거기서 멈추고 더 이상 탐색하지 않음. */}
-          <Route path="/new-post" component={NewPost} />
+          {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
           <Route path="/posts" component={Posts} />
-          <Redirect from="/" to="/posts" />
+          <Route render={() => <h1>Not found</h1>} />
+          {/*<Redirect from="/" to="/posts" />*/}
           {/*<Route path="/" component={Posts} />*/}
         </Switch>
       </div>
