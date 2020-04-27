266a94938aa13311688b2ed34b653e4efcaf0c97
coma.park <psw0946@snu.ac.kr>
2020-04-27 22:17:49 +0900

    React: BurgerBuilder - Routing Pages

--- React/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ React/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -96,29 +96,30 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    this.setState({loading: true});
-    // alert('You continue!')
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Max Schwarzmuller',
-        address: {
-          street: 'TestStreet 1',
-          zipCode: '41351',
-          country: 'Germany'
-        },
-        email: 'test@test.com'
-      },
-      deliveryMethod: 'fastest'
-    }
-    axios.post('/orders.json  ', order)
-      .then(response => {
-        this.setState({loading: false, purchasing: false});
-      })
-      .catch(error => {
-        this.setState({loading: false, purchasing: false});
-      });
+    // this.setState({loading: true});
+    // // alert('You continue!')
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: 'Max Schwarzmuller',
+    //     address: {
+    //       street: 'TestStreet 1',
+    //       zipCode: '41351',
+    //       country: 'Germany'
+    //     },
+    //     email: 'test@test.com'
+    //   },
+    //   deliveryMethod: 'fastest'
+    // }
+    // axios.post('/orders.json  ', order)
+    //   .then(response => {
+    //     this.setState({loading: false, purchasing: false});
+    //   })
+    //   .catch(error => {
+    //     this.setState({loading: false, purchasing: false});
+    //   });
+    this.props.history.push('/checkout');
   };
 
   render() {
