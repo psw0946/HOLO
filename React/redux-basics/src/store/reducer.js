f3ccdb2e7dc062fde7d5471811ca1d66f3ff0821
coma.park <psw0946@snu.ac.kr>
2020-05-09 01:29:33 +0900

    React: Redux - Updating Array Immutably while Implementing Deleting Result

--- React/redux-basics/src/store/reducer.js
+++ React/redux-basics/src/store/reducer.js
@@ -29,6 +29,15 @@ const reducer = (state=initialState, action) => {
         ...state,
         results: state.results.concat({id: new Date(), value: state.counter}), // concat 은 새로운 array 를 만들어내기 때문에 immutable 하다. (push 절대 쓰면 안됨)
       }
+    case 'DELETE_RESULT':
+      // const id = 2;
+      // const newArray = [...state.results];
+      // newArray.splice(id, 1);
+      const updatedArray = state.results.filter(result => result.id !== action.resultElId); // filter 도 새로운 array 를 만들어줌.
+      return {
+        ...state,
+        results: updatedArray,
+      }
   }
   return state;
 };
