ccb5332899234fd31357e143731958ed94198d84
coma.park <psw0946@snu.ac.kr>
2020-05-09 01:12:29 +0900

    React: Redux - Updating State Immutably while Implementing Storing Result

--- React/redux-basics/src/containers/Counter/Counter.js
+++ React/redux-basics/src/containers/Counter/Counter.js
@@ -31,9 +31,16 @@ class Counter extends Component {
             <div>
                 <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
-                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
-                <CounterControl label="Add 10" clicked={this.props.onAddCounter}  />
-                <CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter}  />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter} />
+                <CounterControl label="Add 10" clicked={this.props.onAddCounter} />
+                <CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter} />
+                <hr />
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {this.props.storedResults.map(strResult =>
+                        <li key={strResult.id} onClick={this.props.onDeleteResult}>{strResult.value}</li>
+                    )}
+                </ul>
             </div>
         );
     }
@@ -42,6 +49,7 @@ class Counter extends Component {
 const mapStateToProps = state => {
     return {
         ctr: state.counter,
+        storedResults: state.results,
     };
 };
 
@@ -51,6 +59,8 @@ const mapDispatchToProps = dispatch => {
         onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
         onAddCounter: () => dispatch({type: 'ADD', val: 10}),
         onSubtractCounter: () => dispatch({type: 'SUBTRACT', val: 15}),
+        onStoreResult: () => dispatch({type: 'STORE_RESULT'}),
+        onDeleteResult: () => dispatch({type: 'DELETE_RESULT'})
     };
 };
 
--- React/redux-basics/src/store/reducer.js
+++ React/redux-basics/src/store/reducer.js
@@ -1,25 +1,34 @@
 const initialState = {
   counter: 0,
+  results: [],
 };
 
 const reducer = (state=initialState, action) => {
   switch (action.type) {
     case 'INCREMENT':
-      return {
-        counter: state.counter + 1,
-      };
+      const newState = Object.assign({}, state);
+      newState.counter = state.counter + 1;
+      return newState;
     case 'DECREMENT':
       return {
+        ...state,
         counter: state.counter - 1,
       };
     case 'ADD':
       return {
+        ...state,
         counter: state.counter + action.val,
       };
     case 'SUBTRACT':
       return {
+        ...state,
         counter: state.counter - action.val,
       };
+    case 'STORE_RESULT':
+      return {
+        ...state,
+        results: state.results.concat({id: new Date(), value: state.counter}), // concat 은 새로운 array 를 만들어내기 때문에 immutable 하다. (push 절대 쓰면 안됨)
+      }
   }
   return state;
 };
