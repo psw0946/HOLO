27794ebabdfeacae58f30147721a0b113cd7ca05
coma.park <psw0946@snu.ac.kr>
2020-05-12 21:43:21 +0900

    React: Redux - Simplify Reducer with Utility Function

--- React/redux-basics/src/store/reducers/result.js
+++ React/redux-basics/src/store/reducers/result.js
@@ -1,25 +1,21 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
   results: [],
 };
 
+const deleteResult = (state, action) => {
+  const updatedArray = state.results.filter(result => result.id !== action.resultElId); // filter 도 새로운 array 를 만들어줌.
+  return updateObject(state, {results: updatedArray});
+};
+
 const reducer = (state=initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
-      return {
-        ...state,
-        results: state.results.concat({id: new Date(), value: action.result}), // concat 은 새로운 array 를 만들어내기 때문에 immutable 하다. (push 절대 쓰면 안됨)
-      }
+      return updateObject(state, {results: state.results.concat({id: new Date(), value: action.result})}); // concat 은 새로운 array 를 만들어내기 때문에 immutable 하다. (push 절대 쓰면 안됨)
     case actionTypes.DELETE_RESULT:
-      // const id = 2;
-      // const newArray = [...state.results];
-      // newArray.splice(id, 1);
-      const updatedArray = state.results.filter(result => result.id !== action.resultElId); // filter 도 새로운 array 를 만들어줌.
-      return {
-        ...state,
-        results: updatedArray,
-      }
+      return deleteResult(state, action);
   }
   return state;
 };
