b42cc1fd7e4c59d7f5c96e9bdfd096f5f7fd2e3c
coma.park <psw0946@snu.ac.kr>
2021-01-17 21:18:04 +0900

    Docker: complex project backup for docker practice

---
+++ Docker/complex-elastic-beanstalk/worker/index.js
@@ -0,0 +1,19 @@
+const keys = require('./keys');
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000, // if it loses connection to redis server, it should attempt to reconnect every 1 second
+});
+const sub = redisClient.duplicate();
+
+function fib(index) {
+  if (index < 2) return 1;
+  return fib(index - 1) + fib(index - 2);
+}
+
+sub.on('message', (channel, message) => {
+  redisClient.hset('values', message, fib(parseInt(message))); // key, value
+});
+sub.subscribe('insert');
